943f5ea18875e33eaf60fa6084ac2b7e58846610
Jordi Poch <jordi.poch@edreamsodigeo.com>
2022-12-04 20:08:23 +0100

    Create project structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # advent-of-code-2022
-This is the repo to solve the Advent of Code 2022 using the Java language
+This repo contains my coded solutions for the Advent of Code 2022 using the Java language
---
+++ src/main/java/com/challenge/aoc2022/day1/Day1.java
@@ -0,0 +1,12 @@
+package com.challenge.aoc2022.day1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Day1 {
+    private static final Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        logger.info("Hello world!");
+    }
+}
